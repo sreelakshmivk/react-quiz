0bfab586d82558ae1d240a80a2ff8000f68397bb
Sreelakshmi Kammath <skammath@omnissa.com>
2026-02-11 18:02:57 +0530

    Build issue fix

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -8,7 +8,7 @@ const Answer = ({
 }) => {
   const letterMapping = ["A", "B", "C", "D"];
   const isCorrectAnswer = currentAnswer && answerText === correctAnswer;
-  const isWrongAnswer = currentAnswer == answerText && currentAnswer !== correctAnswer;
+  const isWrongAnswer = currentAnswer === answerText && currentAnswer !== correctAnswer;
   const correctAnswerClass = isCorrectAnswer ? "correct-answer" : "";
   const wrongAnswerClass = isWrongAnswer ? "wrong-answer" : "";
   const disabledClass = currentAnswer ? "disabled-answer" : "";
